c6bb5c94ff243dca660c3daa27ceb76c18aea245
Khalid Richards <columbiakrich@gmail.com>
2015-02-16 10:16:24 -0800

    Sample code for REcycle Rush

---
+++ RecyrushDev/src/org/usfirst/frc/team5770/robot/subsystems/Forklift.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team5770.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Forklift extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	private Talon forkliftMotor;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
